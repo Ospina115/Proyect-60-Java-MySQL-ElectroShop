ea618a8d45735cc9de3d6045b4c055f78575601b
Ospina115 <158192117+Ospina115@users.noreply.github.com>
2024-09-08 10:08:12 -0500

    :ambulance: Fix coding errors
    dios mio casi que no

--- src/main/java/com/tiendaelectrica/Main.java
+++ src/main/java/com/tiendaelectrica/Main.java
@@ -1,6 +1,6 @@
 package com.tiendaelectrica;
 
-import com.tiendaelectrica.menus.menuprincipal.menuprincipal;
+import com.tiendaelectrica.menuprincipal.menuprincipal;
 
 public class Main {
     public static void main(String args[]) {
--- target/classes/database/ddl.sql
+++ target/classes/database/ddl.sql
@@ -6,108 +6,166 @@
 CREATE DATABASE tiendaelectrica;
 USE tiendaelectrica;
 
--- Creacion de la tabla categorias
+
+--          TABLAS PRIMARIAS          --
+
+-- Crear tabla categorias
 CREATE TABLE categorias (
-    id_categoria INT PRIMARY KEY,
+    id_categoria INT PRIMARY KEY AUTO_INCREMENT,
     nombre_categoria VARCHAR(50) NOT NULL
 );
 
--- Creacion de la tabla productos
+-- Crear tabla pais
+CREATE TABLE pais (
+    id_pais INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL
+);
+
+-- Crear tabla direccion_cliente
+CREATE TABLE direccion_cliente (
+    id_direccion INT PRIMARY KEY AUTO_INCREMENT,
+    direccion VARCHAR(50) NOT NULL
+);
+
+-- Crear tabla proveedores
+CREATE TABLE proveedores (
+    id_proveedor INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_proveedor VARCHAR(60) NOT NULL,
+    apellido_proveedor VARCHAR(60) NOT NULL,
+    email_proveedor VARCHAR(100) NOT NULL,
+    telefono_proveedor VARCHAR(15) NOT NULL,
+    direccion_proveedor TEXT NOT NULL
+);
+
+--          TABLAS SECUNDARIAS          --
+
+-- Crear tabla clientes
+CREATE TABLE clientes (
+    id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_cliente VARCHAR(60) NOT NULL,
+    apellido_cliente VARCHAR(60) NOT NULL,
+    email_cliente VARCHAR(100) NOT NULL,
+    telefono_cliente VARCHAR(15) NOT NULL,
+    id_direccioncliente INT,
+    FOREIGN KEY (id_direccioncliente) REFERENCES direccioncliente(id_direccion)
+);
+
+-- Crear tabla productos
 CREATE TABLE productos (
-    id_producto INT PRIMARY KEY,
+    id_producto INT PRIMARY KEY AUTO_INCREMENT,
     nombre_producto VARCHAR(100) NOT NULL,
-    descripcion_producto TEXT,
+    descripcion_producto TEXT NOT NULL,
     precio_producto DECIMAL(10, 2) NOT NULL,
     id_categoria INT,
     FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria)
 );
 
--- Creacion de la tabla inventario
-CREATE TABLE inventario (
-    id_inventario INT PRIMARY KEY,
-    id_producto INT,
-    cantidad_inventario INT NOT NULL,
-    fecha_ultimo_ingreso DATETIME,
-    fecha_ultimo_egreso DATETIME,
-    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
+-- Crear tabla ciudad
+CREATE TABLE ciudad (
+    id_ciudad INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    id_pais INT,
+    FOREIGN KEY (id_pais) REFERENCES pais(id_pais)
 );
 
--- Creacion de la tabla proveedores
-CREATE TABLE proveedores (
-    id_proveedor INT PRIMARY KEY,
-    nombre_proveedor VARCHAR(100) NOT NULL,
-    direccion_proveedor TEXT,
-    telefono_proveedor VARCHAR(20) NOT NULL,
-    email_proveedor VARCHAR(100) NOT NULL
-);
-
--- Creacion de la tabla compras
-CREATE TABLE compras (
-    id_compra INT PRIMARY KEY,
+-- Crear tabla compra
+CREATE TABLE compra (
+    id_compra INT PRIMARY KEY AUTO_INCREMENT,
     id_proveedor INT,
-    fecha_compra DATETIME NOT NULL,
-    total_compra DECIMAL(10, 2) NOT NULL,
+    fecha_compra DATE NOT NULL,
+    precio_compra INT NOT NULL,
     FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor)
 );
 
--- Creacion de la tabla detalles de compras
-CREATE TABLE detalles_compras (
-    id_detalle_compra INT PRIMARY KEY,
+-- Crear tabla detalles_compra
+CREATE TABLE detalles_compra (
+    id_detalles_compra INT PRIMARY KEY AUTO_INCREMENT,
     id_compra INT,
     id_producto INT,
-    cantidad_detalle_compra INT NOT NULL,
-    precio_detalle_compra DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (id_compra) REFERENCES compras(id_compra),
+    nombre_compra VARCHAR(50) NOT NULL,
+    detalles VARCHAR(100) NOT NULL,
+    cantidad INT NOT NULL,
+    precio_unidad INT NOT NULL,
+    FOREIGN KEY (id_compra) REFERENCES compra(id_compra),
     FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
 );
 
--- Creacion de la tabla clientes
-CREATE TABLE clientes (
-    id_cliente INT PRIMARY KEY,
-    nombre_cliente VARCHAR(100) NOT NULL,
-    apellido_cliente VARCHAR(100) NOT NULL,
-    email_cliente VARCHAR(100) NOT NULL,
-    telefono_cliente VARCHAR(20) NOT NULL,
-    direccion_cliente TEXT
+-- Crear tabla inventario
+CREATE TABLE inventario (
+    id_inventario INT PRIMARY KEY AUTO_INCREMENT,
+    id_producto INT,
+    cantidad_inventario INT NOT NULL,
+    fecha_ingreso DATETIME NOT NULL,
+    fecha_actualizacion DATETIME NOT NULL,
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
 );
 
--- Creacion de la tabla ventas
+-- Crear tabla ventas
 CREATE TABLE ventas (
-    id_venta INT PRIMARY KEY,
+    id_venta INT PRIMARY KEY AUTO_INCREMENT,
     id_cliente INT,
     fecha_venta DATETIME NOT NULL,
-    total_venta DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
 
--- Creacion de la tabla detalles_ventas
+-- Crear tabla detalles_ventas
 CREATE TABLE detalles_ventas (
-    id_detalle_venta INT PRIMARY KEY,
+    id_detalle_venta INT PRIMARY KEY AUTO_INCREMENT,
     id_venta INT,
     id_producto INT,
-    cantidad_detalle_venta INT NOT NULL,
-    precio_detalle_venta DECIMAL(10, 2) NOT NULL,
+    cantidad_productos INT NOT NULL,
+    subprecio DECIMAL(10, 2) NOT NULL,
+    descuento INT NOT NULL,
+    total DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (id_venta) REFERENCES ventas(id_venta),
     FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
 );
 
--- Creacion de la tabla pedidos
+-- Crear tabla pedidos
 CREATE TABLE pedidos (
-    id_pedido INT PRIMARY KEY,
+    id_pedido INT PRIMARY KEY AUTO_INCREMENT,
     id_cliente INT,
     fecha_pedido DATETIME NOT NULL,
     estado_pedido VARCHAR(20) NOT NULL,
     FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
 
--- Creacion de la tabla detalles_pedidos
+-- Crear tabla detalles_pedidos
 CREATE TABLE detalles_pedidos (
-    id_detalle_pedido INT PRIMARY KEY,
+    id_detalle_pedido INT PRIMARY KEY AUTO_INCREMENT,
     id_pedido INT,
     id_producto INT,
     cantidad_detalle_pedido INT NOT NULL,
+    descuento INT NOT NULL,
     precio_detalle_pedido DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido),
     FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
 );
 
+-- Crear tabla bodega
+CREATE TABLE bodega (
+    id_bodega INT PRIMARY KEY AUTO_INCREMENT,
+    id_inventario INT,
+    FOREIGN KEY (id_inventario) REFERENCES inventario(id_inventario)
+);
+
+-- Crear tabla sucursal
+CREATE TABLE sucursal (
+    id_sucursal INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    direccion VARCHAR(30) NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    id_ciudad INT,
+    id_producto INT,
+    FOREIGN KEY (id_ciudad) REFERENCES ciudad(id_ciudad),
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
+);
+
+-- Crear tabla producto_proveedor
+CREATE TABLE producto_proveedor (
+    id_producto INT,
+    id_proveedor INT,
+    PRIMARY KEY (id_producto, id_proveedor),
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto),
+    FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor)
+);
